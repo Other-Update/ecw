b6cc31c24eceb674bffcb6a00f6152da7ceaa2a3
Ilaiya <yoursilaiyar@gmail.com>
2017-07-26 22:34:07 +0530

    .co.in to .info

--- ECWorld/www/AdminWorld/index.php
+++ ECWorld/www/AdminWorld/index.php
@@ -5,5 +5,5 @@ function redirect($url, $statusCode = 303)
    header('Location: ' . $url, true, $statusCode);
    die();
 }
-redirect('http://www.ecworld.co.in/AdminWorld/Login/');
+redirect('http://www.ecworld.info/AdminWorld/Login/');
 ?>
