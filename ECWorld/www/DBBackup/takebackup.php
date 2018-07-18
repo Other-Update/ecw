325084b87c11811e4ee2026068d6371f5691ae00
Ilaiya <yoursilaiyar@gmail.com>
2018-07-19 02:28:29 +0530

    Take backup and Clean up the transaction data

--- ECWorld/www/DBBackup/takebackup.php
+++ ECWorld/www/DBBackup/takebackup.php
@@ -1,48 +1,19 @@
 <?php
-$username ="root";
+//This script will take backup of schema,data, triggers but not stored procedures
+$username ="";
 $password ="";
 $hostname = "localhost";
-$database = "ecworld";
+$database = "";
 $username =escapeshellcmd($username);
 $password =escapeshellcmd($password);
 $hostname =escapeshellcmd($hostname);
 $database =escapeshellcmd($database);
-//$backupFile=’/home/www/example.com/backuprestricted/’.date(“Y-m-d-H-i-s”).$database.’.sql’;
-//$backupFile="G:/wamp/www/ECWorldBB/ECWorldwww/DBBackup/".date("Y-m-d-H-i-s").$database.".sql";
-$backupFile=getcwd()."\\".date("Y-m-d-H-i-s").$database.".sql";
-$command = "mysqldump -u $username -p$password $database > $backupFile 2>&1";
+//$backupFile=getcwd()."/".date("Y-m-d-H-i-s").$database.".sql";//Works in godaddy server
+$backupFile="C:/ECWorld/DBBackup/".date("Y-m-d-H-i-s").$database.".sql";//Change path according the server
+$command = "mysqldump -u $username -p$password $database > $backupFile";
 echo $command;echo "\n";
 system($command, $result);
 echo $result;
 
 
- 
-/* 
- * This script only works on linux.
- * It keeps only 31 backups of past 31 days, and backups of each 1st day of past months.
- */
- /*
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'ecworld');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('BACKUP_SAVE_TO', 'G:/wamp/www/ECWorldBB/ECWorldwww/DBBackup'); // without trailing slash
- 
-$time = time();
-$day = date('j', $time);
-if ($day == 1) {
-    $date = date('Y-m-d', $time);
-} else {
-    $date = $day;
-}
- 
-$backupFile = BACKUP_SAVE_TO . '/' . DB_NAME . '_' . $date . '.gz';
-if (file_exists($backupFile)) {
-    unlink($backupFile);
-}
-$command = 'mysqldump --opt -h ' . DB_HOST . ' -u ' . DB_USER . ' -p\'' . DB_PASSWORD . '\' ' . DB_NAME . ' | gzip > ' . $backupFile;
-
-system($command, $result);
-echo $result; 
-*/
 ?>
--- ECWorld/www/admin.ecworld/Online/Misc/CleanupTransactions.php
+++ ECWorld/www/admin.ecworld/Online/Misc/CleanupTransactions.php
@@ -13,14 +13,11 @@ include_once APPROOT_URL.'/Business/b_generalsettings.php';
 include_once APPROOT_URL.'/Business/b_transaction.php';
 include_once APPROOT_URL.'/Business/b_payment.php';
 
-//http://localhost/ECWorldBB/ECWorld/www/admin.ecworld/Online/Login.php?Name=1234567890&Password=Ecw!321&RememberMe=0
+//Get Token using the following url
+//http://localhost/ECWorldBB/ECWorld/www/admin.ecworld/Online/Login.php?Name=&Password=&RememberMe=0
 
-//http://localhost/ECWorldBB/ECWorld/www/admin.ecworld/Online/Misc/CleanupTransactions.php?DeleteOnAndBefore=2017-11-01&Token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MTA3NjE4NDAsImp0aSI6ImNua01GRSt0V1E0YlA4Vmtodmg5Vm10RkhGdnhkRGloaHBxTnVld3pPbjA9IiwiaXNzIjoiRUNXU2VydmVyIiwibmJmIjoxNTEwNzYxODQwLCJleHAiOjE1MTA3OTc4NDAsImRhdGEiOnsidXNlcklkIjoiMSIsInVzZXJOYW1lIjoiQWRtaW4ifX0.Nukan0ie02l05_wqxJJMludRYIdcrajrjxJHO7YFuOk
-
-//To set IST time in all functions by default
-//date_default_timezone_set('Asia/Calcutta');
-$doCheckToken = false; //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODg3MzUxODMsImp0aSI6InFSSWdqRkdibkx4UnJGaXI4cjVEd2ZtZ1FUS3BcL0MzclRSKzM1TnJyNzJjPSIsImlzcyI6IkVDV1NlcnZlciIsIm5iZiI6MTQ4ODczNTE4MywiZXhwIjoxODQ4NzM1MTgzLCJkYXRhIjp7InVzZXJJZCI6IjEiLCJ1c2VyTmFtZSI6IkFkbWluIn19.aftWxYdjc022nEd-7AqEQIY2qivmB4_w1tX7jNWj9bc
-//Develper - //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MDA3NTIxMzUsImp0aSI6InBncVZmdVk1NU5lMkFQMFJDSVdmbmp6K1hveFU4ZUxNWnMzY05taE5tM2s9IiwiaXNzIjoiRUNXU2VydmVyIiwibmJmIjoxNTAwNzUyMTM1LCJleHAiOjM2MDE1MDA3NTIxMzUsImRhdGEiOnsidXNlcklkIjoiMTA4IiwidXNlck5hbWUiOiJEZXZlbG9wZXJzIn19.EivZhpiLVCgnVFhAZLaMLWN3vPcE8RcVH-LQ2PSB0rU
+//DeleteOnAndBefore - YYYY-MM-DD
+//http://admin.ecworld.info/Online/Misc/CleanupTransactions.php?DeleteOnAndBefore=2017-11-01&Token=
 function DeleteOldData($userid,$deleteOnAndBefore){
 	echo "<br/>".$thisuser->DisplayID."-".$thisuser->Name.",";
 	$tObj->DeleteOldTransactions($userid,$deleteOnAndBefore);
@@ -42,9 +39,7 @@ try{
 	$tokenEnc="";
 	//echo $generalConfig["isdev"].",";
 	if($generalConfig["isdev"]==1){
-		//echo "1 year token";
-		//Using 1 year valid admin token for testing.
-		$tokenEnc = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE0ODg3MzUxODMsImp0aSI6InFSSWdqRkdibkx4UnJGaXI4cjVEd2ZtZ1FUS3BcL0MzclRSKzM1TnJyNzJjPSIsImlzcyI6IkVDV1NlcnZlciIsIm5iZiI6MTQ4ODczNTE4MywiZXhwIjoxODQ4NzM1MTgzLCJkYXRhIjp7InVzZXJJZCI6IjEiLCJ1c2VyTmFtZSI6IkFkbWluIn19.aftWxYdjc022nEd-7AqEQIY2qivmB4_w1tX7jNWj9bc";
+		$tokenEnc = "";
 	}else{
 		//echo "url token";
 		$tokenEnc = $_GET['Token'];
@@ -59,15 +54,10 @@ try{
 		echo json_encode($resultObj);
 		return false;
 	}
-	//echo "<br/>token=".json_encode($token->data->userId);
 	
 	$userObj = new b_users("",$mysqlObj,"");
 	$user = $userObj->getByID($token->data->userId);//TODO: get user id from token
 	if($user->UserID!='1') return false;
-	//echo "<br/> userObj=".json_encode($me);
-	//echo "<br/> User=".json_encode($user);
-	//echo "<br/> Action=".$Action;
-	//echo "Ilaiya";
 	
 	$tObj = new b_transaction($user,$mysqlObj,"");
 	$reqObj = new b_request($user,$mysqlObj,"");
@@ -78,9 +68,12 @@ try{
 	$allUsers=$userObj->getAllUsers('1',true,true,'0');
 	//echo json_encode($allUsers);
 	foreach ($allUsers as $thisuser) {
-		DeleteOldData($thisuser->UserID,$deleteOnAndBefore);
+    	echo "<br/>".$thisuser->DisplayID."-".$thisuser->Name.",";
+    	$tObj->DeleteOldTransactions($thisuser->UserID,$deleteOnAndBefore);
+    	$pObj->DeleteOldPayments($thisuser->UserID,$deleteOnAndBefore);
+    	$rcObj->DeleteOldRecharges($thisuser->UserID,$deleteOnAndBefore);
+    	$reqObj->DeleteOldRequests($thisuser->UserID,$deleteOnAndBefore);
 	}
-	//$tObj->cleanupTransactions($user->UserID,$deleteOnAndBefore);
 	
 }catch(Exception $ex){
 	$resultObj = new httpresult();	
